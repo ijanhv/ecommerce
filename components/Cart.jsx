b305ab025bd35c2e8c85bf79b3e6bcf6ca55dc94
Janhavi <patil.janhavi1207@gmail.com>
2022-10-08 19:12:20 +0530

    integrated stripe

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,18 +1,42 @@
 import React, { useRef, useState } from 'react'
+import axios from 'axios'
 import Link from 'next/link';
 import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
 import { TiDeleteOutline } from 'react-icons/ti'
 import toast from 'react-hot-toast'
-
+import getStripe from '../lib/getStripe';
 import { useStateContext } from '../context/StateContext';
 import { urlFor } from '../lib/client';
 
-const Cart = () => {
+const Cart = ({ lineItems, mode, submitType }) => {
+
   const cartRef = useRef();
   const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity, onRemove } = useStateContext();
+  
+  const handleCheckout = async () => {
+    const stripe = await getStripe();
+
+
+    const response = await fetch('/api/stripe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cartItems),
+    });
+
+    if(response.statusCode === 500) return;
+    
+    const data = await response.json();
+
+    toast.loading('Redirecting...');
+
+    
+    stripe.redirectToCheckout({ sessionId: data.id, lineItems, mode, submitType });
+  }
 
   return (
-    <div className='cart-wrapper'>
+    <div className='cart-wrapper' ref={cartRef}>
       <div className='cart-container'>
         <button type='button' className='cart-heading' onClick={() => setShowCart(false)}>
           <AiOutlineLeft />
@@ -64,7 +88,7 @@ const Cart = () => {
               <h3>${totalPrice}</h3>
             </div>
             <div className='btn-container'>
-              <button type='button' className='btn' onClick=''>
+              <button type='button' className='btn' onClick={handleCheckout}>
                 Pay with Stripe
               </button>
             </div>
