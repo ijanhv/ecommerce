8cce60021c78fd9046f4c3f81fcd5a5608e29d66
Janhavi <patil.janhavi1207@gmail.com>
2022-10-08 13:30:50 +0530

    implemented cart logic

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,27 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { client, urlFor } from '../../lib/client'
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai'
 import Product from '../../components/Product';
+import { useStateContext } from '../../context/StateContext.js';
 
 const ProductDetails = ({ product, products }) => {
 
     const { image, name, details, price } = product;
+    const [ index, setIndex ] = useState(0);
+    const { decQty, incQty, qty, onAdd } = useStateContext();
+
   return (
     <div>
         <div className='product-detail-container'>
             <div>
                 <div className='image-container'>
-                    <img src={urlFor(image && image[0])} />
+                    <img src={urlFor(image && image[index])} className='product-detail-image'/>
                 </div>
-                {/* <div className='small-images-container'>
+                <div className="small-images-container">
                     {image?.map((item, i) => (
-                        <img 
+                    <img 
+                        key={i}
                         src={urlFor(item)}
-                        className=""
-                        onMouseEnter=""
+                        className={i === index ? 'small-image selected-image' : 'small-image'}
+                        onMouseEnter={() => setIndex(i)}
                     />
                     ))}
-                </div> */}
+                </div>
 
             </div>
             <div className='product-detail-desc'>
@@ -42,13 +47,13 @@ const ProductDetails = ({ product, products }) => {
                 <div className='quantity'>
                     <h3>Quantity:</h3>
                     <p className='quantity-desc'>
-                        <span className='minus' onClick=""><AiOutlineMinus /></span>
-                        <span className='num' onClick="">0</span>
-                        <span className='plus' onClick=""><AiOutlinePlus /></span>
+                        <span className='minus' onClick={decQty}><AiOutlineMinus /></span>
+                        <span className='num' onClick="">{qty}</span>
+                        <span className='plus' onClick={incQty}><AiOutlinePlus /></span>
                     </p>
                 </div>
                 <div className='buttons'>
-                    <button type="button" className='add-to-cart' onClick="">Add to Cart</button>
+                    <button type="button" className='add-to-cart' onClick={() => onAdd(product, qty)}>Add to Cart</button>
                     <button type="button" className='buy-now' onClick="">Buy Now</button>
                 </div>
             </div>
@@ -95,7 +100,7 @@ export const getStaticProps = async ({ params: { slug }}) => {
     const product = await client.fetch(query);
     const products = await client.fetch(productsQuery);
   
-    console.log(product);
+    // console.log(product);
   
     return {
       props: { products, product }
